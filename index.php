7f5b6c26a1b90f18bdb5e47b2b5661e19e60fee6
bayapucg <bayapu.edu004@gmail.com>
2018-10-05 12:59:26 +0530

    commit
    changes

--- index.php
+++ index.php
@@ -3,63 +3,63 @@
     <title>MedSpace</title>
     <meta name="description" content="" />
     <meta name="keywords" content="" />
-
-
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleControls" data-slide-to="1"></li>
-            <li data-target="#carouselExampleControls" data-slide-to="2"></li>
-        </ol>
-        
-        <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active view">
-                <img class="d-block w-100" src="img/img-1.jpg" alt="First slide">
-                <div class="mask d-flex justify-content-center align-items-center">
-                    <!-- Content -->
-                    <div class="text-center white-text mx-5 wow fadeIn">
-                        <h1 class=" h1 h1-style-caption">
-                            Welcome To Medspace
-                        </h1>
-                    </div>
-                    <!-- Content -->
-                </div>
-            </div>
-            <div class="carousel-item view">
-                <img class="d-block w-100" src="img/img-2.jpg" alt="Second slide">
-                <div class="mask d-flex justify-content-center align-items-center" >
-                    <!-- Content -->
-                    <div class="text-center white-text mx-5 wow fadeIn">
-                        <h1 class=" h1 h1-style-caption">
-                            Welcome To Medspace
-                        </h1>
-                    </div>
-                    <!-- Content -->
-                </div>
-            </div>
-            <div class="carousel-item view">
-                <img class="d-block w-100" src="img/img-4.jpg" alt="Third slide">
-                <div class="mask d-flex justify-content-center align-items-center" >
-                    <!-- Content -->
-                    <div class="text-center white-text mx-5 wow fadeIn">
-                        <h1 class=" h1 h1-style-caption">
-                            Welcome To Medspace
-                        </h1>
-                    </div>
-                    <!-- Content -->
-                </div>
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" style="height:80%;">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" style="height:80%;">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+<!--Carousel Wrapper-->
+<div id="carousel-example-2" class="carousel slide carousel-fade body-start" data-ride="carousel">
+  <!--Indicators-->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-2" data-slide-to="1"></li>
+    <li data-target="#carousel-example-2" data-slide-to="2"></li>
+  </ol>
+  <!--/.Indicators-->
+  <!--Slides-->
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <div class="view">
+        <img class="d-block w-100" src="img/img-1.jpg" alt="First slide">
+        <div class="mask rgba-black-light"></div>
+      </div>
+    
     </div>
+    <div class="carousel-item">
+      <!--Mask color-->
+      <div class="view">
+        <img class="d-block w-100" src="img/img-2.jpg" alt="Second slide">
+        <div class="mask rgba-black-light"></div>
+      </div>
+   
+    </div>
+    <div class="carousel-item">
+      <!--Mask color-->
+      <div class="view">
+        <img class="d-block w-100" src="img/img-3.jpg"  alt="Third slide">
+        <div class="mask rgba-black-slight"></div>
+      </div>
+   
+    </div>
+				<div style="position: absolute;left: 40%;top: 40%;transform: translate(-35%,-50%);">
+				<div class="text-center white-text mx-5 wow fadeIn">
+                        <h1 class=" h1 h1-style-caption">
+                            Welcome To Medspace
+                        </h1>
+                    </div>
+                    </div>
+  </div>
+  <!--/.Slides-->
+  <!--Controls-->
+  <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  <!--/.Controls-->
+</div>
+<!--/.Carousel Wrapper-->
+
+   
 
 
     <!--Main layout-->
