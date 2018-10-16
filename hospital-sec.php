1d915582c1751b7b595bed7f0fe5b6efd266051d
cha-ark <37360201+cha-ark@users.noreply.github.com>
2018-10-16 10:31:29 +0530

    hos
    se

--- hospital-sec.php
+++ hospital-sec.php
@@ -1,8 +1,8 @@
 <?php include("header.php"); ?>
 
-    <title>Hospital Managment Software | MedSpace</title>
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
+    <title>Hospital Managment Software in Tirupati | MedSpace</title>
+    <meta name="description" content="Medspace has developed the best hospital management software in Tirupati. It integrates all the information regarding patients, doctors, staff, laboratory, pharmacy and other hospital administrative details." />
+    <meta name="keywords" content="hospital management software, hospital management software in tirupati, hospital management software, hospital management system software in tirupati" />
     
     <!--Main layout-->
     <main class="mt-5">
@@ -10,16 +10,16 @@
 
             <!--Section: Not enough-->
             <section class="pt-5 wow fadeIn">
-                <h2 class=" h3 text-center py-2">Hospital Managment Software</h2>
+                <h2 class=" h3 text-center py-2">Hospital Managment Software in Tirupati</h2>
                 <br>
                 <div class="row mt-1">
 
                     <!--First column-->
                     <div class="col-md-8">
 
-                        <p>Medspace is an advanced hospital information management system mainly focuses on the clinical, administration and financial needs of the hospital. </p>
-                        <p>It is designed to track patient health flow and can be accessed by doctors and health care providers. It is easy to track patient information, prescriptions, laboratory test results, operations, medical history etc.</p>
-                        <p> Medspace also provides health cards to the patients (customers), these health cards will benefit the patient to access medical history.</p>
+                        <p>It is a great privilege to maintain a hospital and to offer treatment for humankind, on the other hand it is a freaking thing to manage the hospital's activity data. To eradicate this problem Medspace has developed the best hospital management software that is highly advanced and covers many modules. This hospital management software is designed to maintain and manage all the aspects in a hospital i.e hospital's activities like administration, information desk, patient regestration, consulting appointment, pharmacy, laboratory, clinical, financial transactions and many other aspects. </p>
+                        
+                        <p> Medspace also provides health cards to medical care seekers and patients, these health cards will benefit the patient to access his/her medical history easily.</p>
                         
                     </div>
                     <!--/First column-->
@@ -35,49 +35,49 @@
                     
                     <div class="col-md-12">
                         <hr class="my-4">
-                        <h3 class="h3 mb-3">Hospital Admin</h3>
+                        <h3 class="h3 mb-3">Hospital Admin Module</h3>
                         <p>
-                            This is the main module of the HIMS. Hospital admin can add resources like doctor, receptionist, pharmacy, lab coordinator, ward management, nurse. Hospital admin can manage departments of hospitals. It can add speciality and assign department to consultant. Maintains ward details.  Patient details, reports are recorded. Dashboard  contains patient analytics like total number of patients, rescheduled patients, new patients. 
+                            This is the main module of the Hospital Information Management System (HIMS). Hospital admin can add resources like doctor,  staff, nurses, receptionist, pharmacy, lab coordinator, in/out patients, pharmacists, ward details, and test reports etc., and can manage departments in the hospital. Through this hospital management software admin can add/remove or modify every part of the information.  
                         </p>
                         <hr class="my-4">
                     </div>
                     
                     <div class="col-md-12">
-                        <h3 class="h3 mb-3">Reception</h3>
+                        <h3 class="h3 mb-3">Reception Module</h3>
                         <p>
-                            Reception module handles in-patient, out-patient registration, appointment scheduling and availability of doctors. This module collects and stores patient profile. It handles enquires about patient’s admission and discharge details with in the hospital. This facilitates patients to reschedule or repeat appointments with doctor. Birth certificates/death certificates/discharge reports can be generated in this module. 
+                            Reception module handles in-patient registration, out-patient registration, appointment scheduling and availability of doctors etc. This module collects and stores patient profile and handles enquires about patient’s admission and discharge details with in the hospital. This facilitates patients to reschedule i.e to prepone or postpone appointments with doctor. Birth certificates/death certificates/discharge reports can be generated in this module. 
                         </p>
                         <hr class="my-4">
                     </div>
                     
                     <div class="col-md-12">
-                        <h3 class="h3 mb-3">Doctor</h3>
+                        <h3 class="h3 mb-3">Doctors Module</h3>
                         <p>
-                            Doctor can view in-patient and out-patient details in the worksheet and can start the consultation. Prescribe necessary examinations. Add diagnosis and prescriptions for the patients and can directly send the prescriptions and laboratory tests to pharmacy, lab coordinator or the patient can be assigned to another doctor. 
+                            Doctor can view in-patient and out-patient details in the worksheet and can proceed for further treatment. Prescribing necessary examinations or tests and diagnosing the deseases. After diagnosis patient will be given prescription and the carbon prescription copy will be sent to the pharmacy. Patient will be given discount on purchasing medicines with Medspace health card. 
                         </p>
                         <hr class="my-4">
                     </div>
                     
                     <div class="col-md-12">
-                        <h3 class="h3 mb-3">Lab Coordinator</h3>
+                        <h3 class="h3 mb-3">Lab Co-ordinator Module</h3>
                         <p>
-                            The  test prescribed by the doctor is automatically send to laboratory. Lab can feed the details of the test results and print the reports. If the lab doesn’t have the prescribed test then a procurement can be sent to the outsource lab. The test results can be downloaded from patients database.
+                            The  test prescribed by the doctor will be automatically updated to the laboratory. After examining and generating reports Lab technician can update the details of the test results and print the reports. If the lab doesn’t have the prescribed test then a procurement will be sent to the outsource lab. The test results will be updated automatically and can be downloaded from patients database through hospitial management software.
                         </p>
                         <hr class="my-4">
                     </div>
                 
                     <div class="col-md-12">
-                        <h3 class="h3 mb-3">Pharmacy</h3>
+                        <h3 class="h3 mb-3">Pharmacy Module</h3>
                         <p>
-                            The medicine prescribed by the doctor is send to the pharmacy. Can view prescriptions and issue medicines to the patients. This module maintains all the medicine information, dosage details etc. Medicines can be uploaded and viewed details. It can be linked to main billing. As patient collects medicines from pharmacy their charges will automatically transfer to patient billing.
+                            The medicine prescribed by the doctor will be send to the pharmacy. The pharmacist can view prescriptions, dosage and issue medicines to the patients. As patient collects medicines from pharmacy the pharmacy charges will be automatically transferd to main billing.  
                         </p>
                         <hr class="my-4">
                     </div>
                     
                     <div class="col-md-12">
-                        <h3 class="h3 mb-3">Ward Management</h3>
+                        <h3 class="h3 mb-3">Ward Management Module</h3>
                         <p>
-                            The ward management module manages in-patient discharge & transfer. This module takes care of room /ward allocation to the patient once the patient is admitted to the IPD. Assign beds to the patients. It deals with the maintenance of the wards, inter- and intra-ward transfers and preparation of the discharge summary. Can view nursing notes.
+                            The ward management module deals with in-patient discharge & transfer. This module takes care of room /ward allocation to the patient. Once the patient is admitted in the IPD, he/she will be assigned room/bed to the patient. Maintenance of the wards, intra-ward transfers, preparation of the discharge summary and nursing notes can be viewed through this hospital management software.
                         </p>
                         <hr class="my-4">
                     </div>
@@ -85,17 +85,17 @@
                     <div class="col-md-12">
                         <h3 class="h3 mb-3">Nurse Station Module</h3>
                         <p>
-                            Nurse station module enables coordination between nursing station and ward. This module helps the nurses to manage laboratory tests, prescriptions and daily observations. Helps in monitoring of patient condition and keeps record of doctor and nurse notes. This module has the facility to request discharge and bed transfer. It has the facility to order medicines, laboratory tests etc. This module has patient details, diagnosis, admitted physician. Patient condition chart gives the statistics of the patient and maintains reports of patients.
+                            Nurse station module enables coordination between nursing station and ward. This module helps the nurses to manage laboratory tests, prescriptions and daily observations. This module helps in monitoring of patient condition and keeps record of doctor and nurse notes. This module has the facility to request discharge, bed transfer, order medicines and laboratory tests etc. Patient condition chart gives overall statistics about the patient and stores reports of the patient.
                         </p>
                         <hr class="my-4">
                     </div>
                     
                     <div class="col-md-12">
-                        <h3 class="h3 mb-3">Operation Theatre /ICU: </h3>
+                        <h3 class="h3 mb-3">Operation Theatre /ICU Module: </h3>
                         <p>
-                            Operation theatre module maintains scheduling of operation theatres, surgery team, patient tracking, operation theatre consumable management. Maintains preoperative and post operative conditions of the patient. Maintains the complete information of surgery including start time, end time, surgery team, notes etc.
+                            Operation theatre module maintains scheduling of operation theatres, surgery team, patient tracking, operation theatre consumable management etc. It maintains a record of pre & post operative conditions of the patient and gives complete information of surgery including start time, end time, surgery team and notes etc.
                         </p>
-                        <p>ICU module has all the details about patients. The patients can be admitted to any of the ICU’s. Patient follow up can be done and the notes are recorded. Medicines can be requested from pharmacy and send lab tests requests to laboratory.
+                        <p>ICU module has all the details about patients. The patients can be admitted to any of the ICU’s. Patient follow up will be done and the notes are recorded. Medicines can be requested from pharmacy and send lab tests requests to laboratory.
                         </p>
                     </div>
                     
