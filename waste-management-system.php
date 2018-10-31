5bb4802d73d57f86f91a98885ae13a2802e446f6
cha-ark <37360201+cha-ark@users.noreply.github.com>
2018-10-31 15:16:02 +0530

    wats
    waste

--- waste-management-system.php
+++ waste-management-system.php
@@ -1,8 +1,8 @@
 <?php include("header.php"); ?>
 
-    <title>Waste Managment Software | MedSpace</title>
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
+    <title>Bio Medical Waste Management Software | MedSpace</title>
+    <meta name="description" content="Medspace has developed the best biomedical waste management system software in Tirupati, that has various facilities to dispose various types of waste. " />
+    <meta name="keywords" content="biomedical waste management system, biomedical waste management software, bmw software" />
 
     <!--Main layout-->
     <main class="mt-5">
@@ -14,38 +14,42 @@
                 <!--Grid row-->
                 <div class="row mt-4">
                     <!--Grid column-->
-                    <div class="col-md-6">
-                        <br><br>
-                        <!-- Main heading -->
-                        <h3 class="h3 mb-3">Waste Mangement Software </h3>
+					<div class="col-md-12 mb-5">
+                        <h3 class="h3 mb-3">About Bio Waste</h3>
                         <p>
-                            Bio-medical Waste Management Rules, 2016 notified on 28.03.2016 and as amended thereof under the Environment (Protection) Act, 1986, stipulates that it is the duty of every Health Care Facility (HCF) to establish a bar code system for bags or containers containing bio-medical waste (BMW) to be sent out of the premises or place for any purpose, by 27.03.2019. Also, Rule 5 of the BMWM Rules, 2016 stipulates that it is the duty of the every Operator of Common Bio-medical Waste Treatment Facility (CBWTF) to establish bar code system for handling of bio-medical waste. </p>
+                            Precaution is better than cure! Maintaining a clear and an infection free environment for the life to bloom to its potential is very important. As a complex of multi disciplinary structure, a hospital consumes lot of resources for appropriate delivery of medical care, and the leftovers from the consumable are turned as hospital waste. This hospital waste is named as bio-medical waste and to maintain the bio-medical waste process, Medspace has developed the best software named "Bio Medical Waste Management Software".
+                        </p>
+                    </div>
+					
+				    <div class="col-md-12 mb-5">
+                        <br>
+                        <!-- Main heading -->
+                        <h3 class="h3 mb-3">Bio Medical Waste Mangement Rules </h3>
+                        <p>
+                            Bio-medical Waste Management Rules, 2016 notified on 28.03.2016 and as amended thereof under the Environment (Protection) Act, 1986, stipulates that it is the duty of every Health Care Facility (HCF) to establish a bar code system for bags or containers containing bio-medical waste (BMW) to be sent out of the premises or place for any purpose, by 27.03.2019. Also, Rule 5 of the BMWM Rules, 2016 stipulates that it is the duty of the every operator of Common Bio-medical Waste Treatment Facility (CBWTF) to establish bar code system for handling of bio-medical waste. </p>
                         <p>
                             These guidelines have been prepared to facilitate and provide guidance to both the Occupier as well as Operator of CBWTF to establish bar code system and also to have uniformity in adoption of the bar code system throughout the country, thereby ensuring effective enforcement of the BMWM Rules, 2016. </p>
 
                     </div>
                     <!--Grid column-->
                     
-                    <div class="col-md-6">
-
-                        <img src="img/barcode.png" class="img-fluid " alt="">
-
-                    </div>
+                    
                     <!--Grid column-->
+					<div class="col-md-12 mb-5">
+                        <h3 class="h3 mb-3">About Bio Medical Waste Management Software</h3>
+                        <p>
+                            Medspace has developed the best biomedical waste management software that tracks biomedical waste using barcode system. It is used to track biomedical waste generated from health care facilities to its ultimate disposal. Material tracking can be easily managed through bar coding.  Health care facilities (HCF) generates barcodes for bags or containers containing biomedical waste. For hospitals having less than 30 no. of beds are not required to scan their bags containing bio-medical waste. Truck driver  scans the hospital barcode and records the waste collection in app and send the details to HCF (Health Care Facilities) via email. Waste acceptance receipt comprising of date, time, no. of bags, total weight of colour coded bags/containers will be sent to the health care facilities by the CBWTF via email.  Whereas, Health Care Facilities (HCF) having 30 or more no. of beds should generate bar codes for all the bags containing bio-medical waste. The waste collected  by the truck driver will be sent to the disposal plant. At the disposal plant each bag will be scanned and generates waste acceptance receipt, this acceptance report will be printed and sent to HCF via email. Daily and monthly waste collection reports can be tracked.
+                        </p>
+                    </div>
                     
                     <div class="col-md-12 mb-4">
-                        <h3 class="h3 mb-3">Admin</h3>
+                        <h3 class="h3 mb-3">Admin Panel</h3>
                         <p>
-                            Admin can add Health care facility, BMW vehicle and CBWTF. Generates bar code for each hospital added. Dashboard  contains total number of health care facilities, CBWTF, BMW vehicles. Bar code or QR code label can be pre-printed directly on the designated colour coded bags /containers, which may be procured by HCF through the Operator of a CBWTF providing services to them. Bar code or QR code labels can be pasted on the designated colour coded bags/ containers, which can be procured by the HCF either through the Operator of a CBWTF providing treatment services to the HCF. Pre-printed bar code label contains colour of bags (yellow, red, blue & white).
+                            Admin can add Health care facility, BMW vehicle & CBWTF, and generates bar code for each hospital. Dashboard  contains total number of health care facilities, CBWTF, BMW vehicles. Bar code or QR code label can be pre-printed directly on the designated colour coded bags /containers, which may be procured by HCF through the Operator of a CBWTF. These codes can be pasted on the designated colour coded bags. Pre-printed bar code label is in different colours usually in yellow, red, blue and white. 
                         </p>
                     </div>
                     
-                    <div class="col-md-12 mb-5">
-                        <h3 class="h3 mb-3">About Waste Management Software</h3>
-                        <p>
-                            The barcode system is used for tracking bio medical waste generated from health care facilities to its ultimate disposal. Material tracking can be easily managed through bar coding.  Health care facility generates barcodes for bags or containers containing biomedical waste. If HCFs having less than 30 no. of beds are not required to scan their bags containing bio-medical waste. Truck driver  scans the hospital barcode and records the waste collection in app and send the details to HCF via email. Waste acceptance receipt comprising of date, time, no. of bags, total weight of colour coded bags/containers will be sent to the health care facilities by the CBWTF via email.  Whereas, Health Care Facilities having 30 or more no. of beds shall have to generate bar codes for all the bags containing bio-medical waste. The waste collected  by the truck driver will be sent to the plant. At  the plant each bag will be scanned and generates waste acceptance receipt. This acceptance report will be printed and sent to HCF via email also. Daily and monthly waste collection reports can be tracked.
-                        </p>
-                    </div>
+                    
 
                 </div>
                 <!--Grid row-->
