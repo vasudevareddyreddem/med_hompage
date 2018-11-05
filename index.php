9f3f4a55b427abdc7eb4794f26c3f19bb93a8e74
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2018-11-05 18:46:48 +0530

    index page links

--- index.php
+++ index.php
@@ -1,203 +1,204 @@
 <?php include("header.php"); ?>
 
-    <title>Hospital Management System in Tirupati - MedSpace</title>
-    <meta name="description" content="Medspace has developed the best hospital management system in Tirupati. It integrates all the information regarding patients, doctors, staff, laboratory, pharmacy and other hospital administrative details." />
-    <meta name="keywords" content="hospital management system, hospital management system in tirupati, hospital management software, hospital management system software in tirupati" />
+<title>Hospital Management System in Tirupati - MedSpace</title>
+<meta name="description" content="Medspace has developed the best hospital management system in Tirupati. It integrates all the information regarding patients, doctors, staff, laboratory, pharmacy and other hospital administrative details." />
+<meta name="keywords" content="hospital management system, hospital management system in tirupati, hospital management software, hospital management system software in tirupati" />
 <!--Carousel Wrapper-->
 <div id="carousel-example-2" class="carousel slide carousel-fade body-start" data-ride="carousel">
-  <!--Indicators-->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-2" data-slide-to="1"></li>
-    <li data-target="#carousel-example-2" data-slide-to="2"></li>
-  </ol>
-  <!--/.Indicators-->
-  <!--Slides-->
-  <div class="carousel-inner" role="listbox">
-    <div class="carousel-item active">
-      <div class="view">
-        <img class="d-block w-100" src="img/img-1.jpg" alt="First slide">
-        <div class="mask rgba-black-light"></div>
-      </div>
-    
+    <!--Indicators-->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-2" data-slide-to="1"></li>
+        <li data-target="#carousel-example-2" data-slide-to="2"></li>
+    </ol>
+    <!--/.Indicators-->
+    <!--Slides-->
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <div class="view">
+                <img class="d-block w-100" src="img/img-1.jpg" alt="First slide">
+                <div class="mask rgba-black-light"></div>
+            </div>
+
+        </div>
+        <div class="carousel-item">
+            <!--Mask color-->
+            <div class="view">
+                <img class="d-block w-100" src="img/img-2.jpg" alt="Second slide">
+                <div class="mask rgba-black-light"></div>
+            </div>
+
+        </div>
+        <div class="carousel-item">
+            <!--Mask color-->
+            <div class="view">
+                <img class="d-block w-100" src="img/img-3.jpg" alt="Third slide">
+                <div class="mask rgba-black-slight"></div>
+            </div>
+
+        </div>
+        <div style="position: absolute;left: 25%;top: 40%;transform: translate(-20%,-50%);">
+            <div class="text-center white-text mx-5 wow fadeIn">
+                <h1 class=" h4 h1-style-caption">
+                    Medspace - Hospital Management System
+                </h1>
+            </div>
+        </div>
     </div>
-    <div class="carousel-item">
-      <!--Mask color-->
-      <div class="view">
-        <img class="d-block w-100" src="img/img-2.jpg" alt="Second slide">
-        <div class="mask rgba-black-light"></div>
-      </div>
-   
-    </div>
-    <div class="carousel-item">
-      <!--Mask color-->
-      <div class="view">
-        <img class="d-block w-100" src="img/img-3.jpg"  alt="Third slide">
-        <div class="mask rgba-black-slight"></div>
-      </div>
-   
-    </div>
-				<div style="position: absolute;left: 25%;top: 40%;transform: translate(-20%,-50%);">
-				<div class="text-center white-text mx-5 wow fadeIn">
-                        <h1 class=" h4 h1-style-caption">
-                             Medspace - Hospital Management System
-                        </h1>
-                    </div>
-                    </div>
-  </div>
-  <!--/.Slides-->
-  <!--Controls-->
-  <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-  <!--/.Controls-->
+    <!--/.Slides-->
+    <!--Controls-->
+    <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    <!--/.Controls-->
 </div>
 <!--/.Carousel Wrapper-->
 
-   
 
 
-    <!--Main layout-->
-    <main class="mt-5">
-        <div class="container pt-5">
-		
-            <!--Section: Main info-->
-            <section>    
-                <!--First row-->
-                <div class="row features-small mb-5 mt-3 wow fadeIn">
 
-                    <!--First column-->
-                    <div class="col-md-8">
-                        
-                        <h3 class="h3 mb-3">About Medspace</h3>
-                        <p>The evolution of technology has made the communication process much easier for humans. In the past decade one has to go physically to the hospital to book a doctor  appointment, but now through the expeditious expansion in software  one can book the doctor’s appointment through mobile app, all you need is a smart phone with an internet connection. We at Medspace Softtech Pvt. Ltd. has developed a hospital management system to benefit both the hospitals and medical care seekers. The company deals with hospital management system, health cards and biomedical waste management system. It helps people to find online medical services like choosing the right hospital, booking doctor  appointment, booking diagnostic center appointment, storing medical records, purchasing online medicines  etc. Medspace Softtech Pvt. Ltd. is located in Hyderabad and has its branches in Tirupati, Guntur and Bangalore and is planning to launch more branches across India. 
- </p>
-                        
-                    </div>
-                    <!--/First column-->
+<!--Main layout-->
+<main class="mt-5">
+    <div class="container pt-5">
 
-                    <!--Second column-->
-                    <div class="col-md-4">
-                        <img src="img/about.png" alt="" class="img-fluid " >
-                    </div>
-                    <!--/Second column-->
-                </div>
+        <!--Section: Main info-->
+        <section>
+            <!--First row-->
+            <div class="row features-small mb-5 mt-3 wow fadeIn">
 
-            </section>
-            <!--Section: Main info-->
+                <!--First column-->
+                <div class="col-md-8">
 
-            <hr class="my-5">
-
-            <!--Section: Not enough-->
-            <section class="wow fadeIn pt-5">
-
-                <!--Grid row-->
-                <div class="row">
-
-                    <!--Grid column-->
-                    <div class="col-md-4 mb-4">
-
-                        <img src="img/hospital-mangement-system.png" class="img-fluid " alt="">
-
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-md-8 mb-4">
-
-                        <!-- Main heading -->
-                        <h3 class="h3 mb-3">Hospital Management System</h3>
-                        <p> Medspace has developed a reliable hospital management system software for the hospitals. It has been designed keeping in mind today's complex health care system requirements and the importance of time.</p>
-						<p> This hospital management software helps you to manage all the functions within the hospital like Reception, In-patient Management, Out-patient Management, Laboratory Management, Pharmacy, Nurse and Doctor records. The main purpose of this hospital management system is to reduce human efforts and to shift the physical records to electronic records.  </p>
-                       <a href=" hospital-sec.php" class="btn btn-primary btn-md navbar-color darken-3">View more</a>
-                    
-
-                    </div>
-                    <!--Grid column-->
+                    <h3 class="h3 mb-3">About Medspace</h3>
+                    <p>The evolution of technology has made the communication process much easier for humans. In the past decade one has to go physically to the hospital to book a doctor appointment, but now through the expeditious expansion in software one can book the doctor’s appointment through mobile app, all you need is a smart phone with an internet connection. We at Medspace Softtech Pvt. Ltd. has developed a hospital management system to benefit both the hospitals and medical care seekers. The company deals with hospital management system, health cards and biomedical waste management system. It helps people to find online medical services like choosing the right hospital, booking doctor appointment, booking diagnostic center appointment, storing medical records, purchasing online medicines etc. Medspace Softtech Pvt. Ltd. is located in Hyderabad and has its branches in Tirupati, Guntur and Bangalore and is planning to launch more branches across India.
+                    </p>
 
                 </div>
-                <!--Grid row-->
+                <!--/First column-->
 
-            </section>
-            
+                <!--Second column-->
+                <div class="col-md-4">
+                    <img src="img/about.png" alt="" class="img-fluid ">
+                </div>
+                <!--/Second column-->
+            </div>
 
-            <hr class="my-5"><section class="wow fadeIn pt-5">
+        </section>
+        <!--Section: Main info-->
 
-                <!--Grid row-->
-                <div class="row">
+        <hr class="my-5">
 
-                   
+        <!--Section: Not enough-->
+        <section class="wow fadeIn pt-5">
 
-                    <!--Grid column-->
-                    <div class="col-md-8 mb-4">
+            <!--Grid row-->
+            <div class="row">
 
-                        <!-- Main heading -->
-                        <h3 class="h3 mb-3">Medspace Health Card</h3>
-                        <p> Apart from hospital management system, Medspace also provides health cards in the form of smart cards which is designed to store patient’s complete medical information. This health card enables medical care seekers and patients to have a faster diagnosis and avoids unnecessary investigations or tests every time they visit the hospital.  </p>
-                        <p> Medspace health card will provide various discounts on laboratory tests and medicines. </p> 
-                        <p>When a patient visits the hospital, his/her information and medical records can be accessed directly through Medspace health card and at the end of every visit latest information is updated to the health card.</p>
-                        
+                <!--Grid column-->
+                <div class="col-md-4 mb-4">
 
-                        <a href="health-card.php" class="btn btn-primary btn-md navbar-color darken-3">View more</a>
-
-                    </div>
-					 
-                    <div class="col-md-4 py-5">
-
-                        <img src="img/card.png" class="img-fluid " alt="">
-
-                    </div>
-                    
-                    <!--Grid column-->
+                    <img src="img/hospital-mangement-system.png" class="img-fluid " alt="">
 
                 </div>
-                <!--Grid row-->
+                <!--Grid column-->
 
-            </section>
-            
+                <!--Grid column-->
+                <div class="col-md-8 mb-4">
 
-            <hr class="my-5">
-            
-            <section class="mt-5 wow fadeIn" id="waste-m-section">
+                    <!-- Main heading -->
+                    <h3 class="h3 mb-3">Hospital Management System</h3>
+                    <p> Medspace has developed a reliable hospital management system software for the hospitals. It has been designed keeping in mind today's complex health care system requirements and the importance of time.</p>
+                    <p> This hospital management software helps you to manage all the functions within the hospital like Reception, In-patient Management, Out-patient Management, Laboratory Management, Pharmacy, Nurse and Doctor records. The main purpose of this hospital management system is to reduce human efforts and to shift the physical records to electronic records. </p>
+                    <a href="hospital-management-software.php" class="btn btn-primary btn-md navbar-color darken-3">View more</a>
 
-                <!--Grid row-->
-                <div class="row">
-                    
-                    <div class="col-md-6 mb-4">
 
-                        <img src="img/barcode.png" class="img-fluid " alt="">
-
-                    </div>
-                    <div class="col-md-6 mb-4">
-
-                        <!-- Main heading -->
-                        <h3 class="h3 mb-3">Waste Mangement Software </h3>
-                        <p>
-                            As per Bio-medical Waste Management Rules, 2016 notified on 28.03.2016 and as amended thereof under the Environment (Protection) Act, 1986, stipulates that it is the duty of every Health Care Facility (HCF) to establish a bar code system for bags or containers containing bio-medical waste (BMW) to be sent out of the premises or place for any purpose, by 27.03.2019. Also, Rule 5 of the BMWM Rules, 2016 stipulates that it is the duty of the every Operator of Common Bio-medical Waste Treatment Facility (CBWTF) to establish bar code system for handling of bio-medical waste. </p>
-                        <p>
-                            As per these guidelines Medspace has developed a waste management software to track the disposal activity from accumulation of waste to disposal of waste.
- </p>
-
-                        <!-- Main heading -->
-                        <a href="waste-mang.php" class="btn btn-primary btn-md navbar-color">View more</a>
-
-                    </div>
-                   
-                    
                 </div>
-                <!--Grid row-->
+                <!--Grid column-->
 
-            </section>
+            </div>
+            <!--Grid row-->
 
-            
+        </section>
 
-        </div>
-    </main>
-    <!--Main layout-->
-    
+
+        <hr class="my-5">
+        <section class="wow fadeIn pt-5">
+
+            <!--Grid row-->
+            <div class="row">
+
+
+
+                <!--Grid column-->
+                <div class="col-md-8 mb-4">
+
+                    <!-- Main heading -->
+                    <h3 class="h3 mb-3">Medspace Health Card</h3>
+                    <p> Apart from hospital management system, Medspace also provides health cards in the form of smart cards which is designed to store patient’s complete medical information. This health card enables medical care seekers and patients to have a faster diagnosis and avoids unnecessary investigations or tests every time they visit the hospital. </p>
+                    <p> Medspace health card will provide various discounts on laboratory tests and medicines. </p>
+                    <p>When a patient visits the hospital, his/her information and medical records can be accessed directly through Medspace health card and at the end of every visit latest information is updated to the health card.</p>
+
+
+                    <a href="healthcards.php" class="btn btn-primary btn-md navbar-color darken-3">View more</a>
+
+                </div>
+
+                <div class="col-md-4 py-5">
+
+                    <img src="img/card.png" class="img-fluid " alt="">
+
+                </div>
+
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+
+        </section>
+
+
+        <hr class="my-5">
+
+        <section class="mt-5 wow fadeIn" id="waste-m-section">
+
+            <!--Grid row-->
+            <div class="row">
+
+                <div class="col-md-6 mb-4">
+
+                    <img src="img/barcode.png" class="img-fluid " alt="">
+
+                </div>
+                <div class="col-md-6 mb-4">
+
+                    <!-- Main heading -->
+                    <h3 class="h3 mb-3">Waste Mangement Software </h3>
+                    <p>
+                        As per Bio-medical Waste Management Rules, 2016 notified on 28.03.2016 and as amended thereof under the Environment (Protection) Act, 1986, stipulates that it is the duty of every Health Care Facility (HCF) to establish a bar code system for bags or containers containing bio-medical waste (BMW) to be sent out of the premises or place for any purpose, by 27.03.2019. Also, Rule 5 of the BMWM Rules, 2016 stipulates that it is the duty of the every Operator of Common Bio-medical Waste Treatment Facility (CBWTF) to establish bar code system for handling of bio-medical waste. </p>
+                    <p>
+                        As per these guidelines Medspace has developed a waste management software to track the disposal activity from accumulation of waste to disposal of waste.
+                    </p>
+
+                    <!-- Main heading -->
+                    <a href="waste-management-system.php" class="btn btn-primary btn-md navbar-color">View more</a>
+
+                </div>
+
+
+            </div>
+            <!--Grid row-->
+
+        </section>
+
+
+
+    </div>
+</main>
+<!--Main layout-->
+
 <?php include("footer.php"); ?>
